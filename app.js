03c75c631f9489af9e3b9f7f0667aa3a2c744706
Alex Les <awles642@gmail.com>
2023-04-09 14:03:13 -0400

    Added connection to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from "cors";
+import mongoose from "mongoose";
 import HelloController from "./controllers/hello-controller.js"
 import UsersController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import cors from "cors";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+
 const app = express();
+mongoose.connect(CONNECTION_STRING);
+
 app.use(cors())
 app.use(express.json());
 TuitsController(app);
